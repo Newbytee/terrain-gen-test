3e5ea5262fd6a7b68bfd80809832f69eb4a9dc2a
Kir Neboula <newbie13xd@outlook.com>
2018-09-12 20:52:37 +0200

    meep

--- script.js
+++ script.js
@@ -1,21 +1,86 @@
+"use strict";
+
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let terrain = []; // = [ 5, 9, 1, 3, 9, 3, 5, 3, 5, 7, 25 ];
-terrain.length = 250;
+let viewport = 0;
+terrain.length = 350;
+generateTerrain();
 
 setInterval(function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    
-    generateTerrain();
-}, 500);
+    drawTerrain();
+}, 100);
 
 function generateTerrain() {
     for (let i = 0; i < terrain.length; i++) {
-        terrain[i] = Math.random() * 20;
-    }
-    
-    for (let i = 0; i < terrain.length; i++) {
-        ctx.fillStyle = "#33FF33";
-        ctx.fillRect(2 * i, canvas.height - 20 - terrain[i], 2, canvas.height);    
+        terrain[i] = generateTerrainPos(i);
     }
 }
+
+function generateTerrainMore(nBlocks) {
+    terrain.length = terrain.length + nBlocks;
+
+    for (let i = 0; i < terrain.length; i++) {
+        if (typeof terrain[i] === "undefined") {
+            terrain[i] = generateTerrainPos(i);
+        }
+    }
+}
+
+function generateTerrainPos(pos) {
+    if (pos === 0) {
+        return Math.random() * 50;
+    } else {
+        let sign;
+        if (Math.random() < 0.5) {
+            sign = -1;
+        } else {
+            sign = 1;
+        }
+        return Math.random() * 3 * sign + terrain[pos - 1];
+    }
+}
+
+function drawTerrain() {
+    ctx.fillStyle = "#000FFF";
+    ctx.fillRect(0, canvas.height - 20, canvas.width, canvas.height);
+    for (let i = viewport; i < terrain.length + viewport; i++) {
+        ctx.fillStyle = "#33FF33";
+        //console.log(i);
+        //if (typeof terrain[i] === "undefined") '
+        //generateTerrainPos(i);
+        ctx.fillRect(2 * (i - viewport), canvas.height - 20 - terrain[i], 2, canvas.height);
+    }
+}
+
+document.addEventListener("keydown", function(event) {
+    switch (event.code) {
+        case "KeyR":
+            generateTerrain();
+            break;
+        case "ArrowLeft":
+            if (viewport > 0) {
+                viewport--;
+            }
+            if (typeof terrain[viewport + 200] === "undefined") {
+                generateTerrainMore(viewport);
+            }
+            break;
+        case "ArrowRight":
+            viewport++;
+            /*if (typeof terrain[terrain.length + viewport] === "undefined") {
+                console.log(terrain.length + viewport);
+                terrain.push(generateTerrainPos(terrain.length + viewport - 1));
+            }*/
+
+            /*if (viewport % 100 === 0) {
+                generateTerrain(viewport)
+            }
+            break;*/
+            if (typeof terrain[viewport + 200] === "undefined") {
+                generateTerrainMore(viewport);
+            }
+            break;
+    }
+});
